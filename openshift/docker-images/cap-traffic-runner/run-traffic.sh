f754b9ff5ae39fabaaa1235783dcb033608a0c0f
Sivanand <poornima.sivanand@gov.bc.ca>
2021-04-08 14:14:14 -0700

    fix

--- openshift/docker-images/cap-traffic-runner/run-traffic.sh
+++ openshift/docker-images/cap-traffic-runner/run-traffic.sh
@@ -7,6 +7,5 @@ do
     eval "eval target_location=\$\{URL${id}\}";
     if [[ '' != ${target_location} ]]; then
         watch -n 15 -d "wget -O /tmp/file${id} --no-check-certificate -p \"${target_location}\"" #background process
-        cat /runner/test${id}.log
     fi
 done
