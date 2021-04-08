16ce6482f962bd55dc2979fd68621be3fd49f046
Sivanand <poornima.sivanand@gov.bc.ca>
2021-04-08 12:09:29 -0700

    fix

--- openshift/docker-images/cap-traffic-runner/run-traffic.sh
+++ openshift/docker-images/cap-traffic-runner/run-traffic.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # copy paste this into the terminal once the setup is done
+
 bash;
-
-
 for id in 1 2 3 
 do
     eval "eval target_location=\$\{URL${id}\}";
     if [[ '' != ${target_location} ]]; then
-        echo ${target_location}
-       #watch -n 15 -d 
-       wget -O /tmp/file${id} -o /dev/null --no-check-certificate -p \"${target_location}\" >> /runner/logs.txt #background process
+        watch -n 15 -d "wget -O /tmp/file${id} -o /runner/test${id}.log --no-check-certificate -p \"${target_location}\""& #background process
+        cat /runner/test${id}.log
     fi
 done
